11a10da5730ab48b425bb781840b58e8681b5ad7
root <root@DESKTOP-HsnMkt>
2025-05-16 16:50:45 +0700

    .../ rm

--- pages/api/[id].ts
+++ pages/api/[id].ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import linkMap from '../../../data/linkMap';
+import linkMap from '../../data/linkMap';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   res.setHeader('Access-Control-Allow-Origin', '*');
