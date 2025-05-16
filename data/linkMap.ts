3f6b285dbad05bd79870c5677c98643201bb370d
root <root@DESKTOP-HsnMkt>
2025-05-16 17:39:16 +0700

    user=prev gogle drifve

--- data/linkMap.ts
+++ data/linkMap.ts
@@ -1,5 +1,5 @@
 const linkMap: Record<string, string> = {
-  DokSekolahPresenSD: 'https://drive.google.com/drive/folders/1xiz5IF0xhsMKqnol0cBrO8KWScXV7cd-/preview',
+  DokSekolahPresenSD: 'https://drive.google.com/drive/folders/1xiz5IF0xhsMKqnol0cBrO8KWScXV7cd-?usp=sharing',
   Doksekolah02052025 : 'https://drive.google.com/drive/folders/1cbe-vzRMiYsxDbWJtbsTghV_wLdNqhav?usp=drive_link',
   // Tambahkan sesuai kebutuhan
 };
