5665f75bd055f190a3cafa1d5514bb586eb45e58
root <root@DESKTOP-HsnMkt>
2025-05-16 14:38:43 +0700

    halah

--- pages/api/location.ts
+++ pages/api/location.ts
@@ -1,5 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import UAParser from 'ua-parser-js';
+
+// @ts-ignore
+const UAParser = require('ua-parser-js');
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   // CORS
