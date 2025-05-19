239a2bde7eef1c9ce23f8a149d0296603b653c5b
root <root@DESKTOP-HsnMkt>
2025-05-19 17:24:25 +0700

    doksekolah17052025

--- data/linkMap.ts
+++ data/linkMap.ts
@@ -1,6 +1,7 @@
 const linkMap: Record<string, string> = {
   DokSekolahPresenSD: 'https://drive.google.com/drive/folders/1xiz5IF0xhsMKqnol0cBrO8KWScXV7cd-?usp=sharing',
-  Doksekolah02052025 : 'https://drive.google.com/drive/folders/1cbe-vzRMiYsxDbWJtbsTghV_wLdNqhav?usp=drive_link',
+  Doksekolah02052025: 'https://drive.google.com/drive/folders/1cbe-vzRMiYsxDbWJtbsTghV_wLdNqhav?usp=drive_link',
+  DokSekolah17052025: 'https://drive.google.com/drive/folders/1lhaKwnRsYwfgBPIjuIbS6tzSQX0p8exa?usp=sharing' 
   // Tambahkan sesuai kebutuhan
 };
 
