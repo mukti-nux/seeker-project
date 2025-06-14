9d2f2194eb90be333b462fe2fb3fb841f3806ce8
root <root@DESKTOP-HsnMkt.localdomain>
2025-06-14 16:53:10 +0700

    benekne id

--- data/linkMap.ts
+++ data/linkMap.ts
@@ -1,5 +1,5 @@
 const linkMap: Record<string, string> = {
-  Doksekolah14062025: 'https://drive.google.com/drive/folders/183_VRxmyxyCMR-v9kivgLuYzVMPdvzRz?usp=sharing',
+  DokSekolah14062025: 'https://drive.google.com/drive/folders/183_VRxmyxyCMR-v9kivgLuYzVMPdvzRz?usp=sharing',
   DokSekolahSD09062025: 'https://drive.google.com/drive/folders/1lx5Zbt66ly6o5Udajllp72GedBd_sxLu?usp=sharing',
   DokSekolahPresenSD: 'https://drive.google.com/drive/folders/1xiz5IF0xhsMKqnol0cBrO8KWScXV7cd-?usp=sharing',
   Doksekolah02052025: 'https://drive.google.com/drive/folders/1cbe-vzRMiYsxDbWJtbsTghV_wLdNqhav?usp=drive_link',
