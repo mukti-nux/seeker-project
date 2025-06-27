cb6f84bb6bed0baad46d04a26c285f93a6586de4
root <root@DESKTOP-HsnMkt.localdomain>
2025-06-27 12:41:46 +0700

    add mustofa ver.

--- data/linkMap.ts
+++ data/linkMap.ts
@@ -1,4 +1,5 @@
 const linkMap: Record<string, string> = {
+  DokSekolah14062025Mus: 'https://drive.google.com/drive/folders/1rN85jFD_lKe-M7Qy844KO0Ar6M8FD3el',
   DokSekolahSD21062025: 'https://drive.google.com/drive/folders/10uCeWk9-pDlPiKT_XwL1kSG77cL9_DnG?usp=sharing',
   DokSekolah14062025: 'https://drive.google.com/drive/folders/183_VRxmyxyCMR-v9kivgLuYzVMPdvzRz?usp=sharing',
   DokSekolahSD09062025: 'https://drive.google.com/drive/folders/1lx5Zbt66ly6o5Udajllp72GedBd_sxLu?usp=sharing',
