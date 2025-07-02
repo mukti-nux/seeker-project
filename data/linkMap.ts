4b786c9f41d4ec62658bbea617f25b49336d09a0
root <root@DESKTOP-HsnMkt.localdomain>
2025-07-02 17:32:46 +0700

    sdd link

--- data/linkMap.ts
+++ data/linkMap.ts
@@ -1,4 +1,5 @@
 const linkMap: Record<string, string> = {
+  LinkPresjumpingTextOutline: 'https://drive.google.com/file/d/1wFE0n0xWtf6Q9ePt46c0hqb8m8lhyWD_/view?usp=sharing',
   LinkPreset001: 'https://alightcreative.com/am/share/u/N8Kg5ZB1gXUSHa442c9K7wKmMUA2/p/21dwCaYbL5-fb5ef3b9f6aaeb14',
   DokSekolah14062025Mus: 'https://drive.google.com/drive/folders/1rN85jFD_lKe-M7Qy844KO0Ar6M8FD3el',
   DokSekolahSD21062025: 'https://drive.google.com/drive/folders/10uCeWk9-pDlPiKT_XwL1kSG77cL9_DnG?usp=sharing',
