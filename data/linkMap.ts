d6e61c8bb08da8b0388cc6aaad7846a463373b5e
root <root@DESKTOP-HsnMkt.localdomain>
2025-07-07 04:04:55 +0700

    add preset AM002

--- data/linkMap.ts
+++ data/linkMap.ts
@@ -1,6 +1,7 @@
 const linkMap: Record<string, string> = {
+  LinkPresetAM002: 'https://alightcreative.com/am/share/u/N8Kg5ZB1gXUSHa442c9K7wKmMUA2/p/tMkSWK9OuD-2f663bb657651b3c',
   LinkPresjumpingTextOutline: 'https://drive.google.com/file/d/1wFE0n0xWtf6Q9ePt46c0hqb8m8lhyWD_/view?usp=sharing',
-  LinkPreset001: 'https://alightcreative.com/am/share/u/N8Kg5ZB1gXUSHa442c9K7wKmMUA2/p/21dwCaYbL5-fb5ef3b9f6aaeb14',
+  LinkPresetAM001: 'https://alightcreative.com/am/share/u/N8Kg5ZB1gXUSHa442c9K7wKmMUA2/p/21dwCaYbL5-fb5ef3b9f6aaeb14',
   DokSekolah14062025Mus: 'https://drive.google.com/drive/folders/1rN85jFD_lKe-M7Qy844KO0Ar6M8FD3el',
   DokSekolahSD21062025: 'https://drive.google.com/drive/folders/10uCeWk9-pDlPiKT_XwL1kSG77cL9_DnG?usp=sharing',
   DokSekolah14062025: 'https://drive.google.com/drive/folders/183_VRxmyxyCMR-v9kivgLuYzVMPdvzRz?usp=sharing',
