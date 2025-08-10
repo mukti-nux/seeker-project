c91cdd89829cb527f44fb9972e2df0177aaa3588
root <root@DESKTOP-HsnMkt.localdomain>
2025-08-10 10:53:23 +0700

    pindah db

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -7,7 +7,7 @@ const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT!);
 if (!getApps().length) {
   initializeApp({
     credential: cert(serviceAccount),
-    databaseURL: 'https://seeker-projectku-default-rtdb.asia-southeast1.firebasedatabase.app',
+    databaseURL: 'https://track-seeker-data-default-rtdb.asia-southeast1.firebasedatabase.app/',
   });
 }
 
